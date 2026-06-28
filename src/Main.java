c42530dfaf660dd849983a6d8a4df71f75969937
smMazibuko <sphiwem715@gmail.com>
2026-06-28 23:42:26 +0200

    App loop created

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,32 @@ void main(){
 
     ArrayList<Task> taskList = new ArrayList<Task>();
     int currentIndex = 0;
-    int userChoice;
+    int userChoice = 1;
     Scanner mainScanner = new Scanner(System.in);
 
     System.out.println("Welcome to Task Tracker.\n");
 
-    System.out.println("Select an option: \n1) Create new task.");
-    userChoice = mainScanner.nextInt();
+    while (userChoice != 3){
 
-    if (userChoice == 1){
-        taskList.add(addTask(currentIndex + 1));
+        System.out.println("Select an option: \n1) View all tasks\n2) Create a new task\n3) End program");
+        userChoice = mainScanner.nextInt();
+
+        if (userChoice == 1) {
+            if (!taskList.isEmpty()) {viewAllTasks(taskList);}
+            else {
+                System.out.println("No tasks found.");
+            };
+        }
+
+        if (userChoice == 2){
+            taskList.add(addTask(currentIndex + 1));
+        }
+
+        if (userChoice < 1 || userChoice >  3) System.out.println("Invalid input.");
+
     }
 
-    // if (!taskList.isEmpty()) viewAllTasks(taskList);
-
+    mainScanner.close();
     System.out.println("\nGoodbye");
 
 }
