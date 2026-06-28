83418b1d3ca59f56451a4f5c44ceb4539a7b2258
smMazibuko <sphiwem715@gmail.com>
2026-06-28 18:31:19 +0200

    Task class created

---
+++ src/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+
+    int id;
+    String description;
+    boolean isComplete;
+
+    Task(int id, String description){
+        this.id = id;
+        this.description = description;
+        this.isComplete = false;
+    }
+
+}
