4f69ab1c76a49f8e6f12b13897131d0e8a284c86
smMazibuko <sphiwem715@gmail.com>
2026-06-29 10:35:36 +0200

    fix: second scanner removed and switch breaks added

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ void main() {
     ArrayList<Task> taskList = new ArrayList<Task>();
     int currentIndex = 0;
     int userChoice = 1;
-    Scanner mainScanner = new Scanner(System.in);
+    Scanner userInput = new Scanner(System.in);
 
     System.out.println("Welcome to Task Tracker.\n");
 
     while (userChoice != 3) {
 
         System.out.println("Select an option: \n1) View all tasks\n2) Create a new task\n3) End program");
-        userChoice = mainScanner.nextInt();
+        userChoice = userInput.nextInt();
+        userInput.nextLine(); // consume newline left over by nextInt()
 
         switch (userChoice) {
             case 1:
@@ -20,29 +21,30 @@ void main() {
                 } else {
                     System.out.println("No tasks found.\n");
                 }
+                break;
 
-            // bug: app breaks after creating one task
             case 2:
-                taskList.add(addTask(currentIndex + 1));
+                taskList.add(addTask(userInput,currentIndex + 1));
+                currentIndex++;
+                break;
 
             case 3:
                 break;
 
             default:
                 System.out.println("Invalid input.\n");
+                break;
         }
 
     }
 
-    mainScanner.close();
+    userInput.close();
     System.out.println("\nGoodbye.");
 
 }
 
 // Creates a new task
-Task addTask(int id) {
-
-    Scanner input = new Scanner(System.in);
+Task addTask(Scanner input, int id) {
 
     System.out.println("\nDescribe your new task:");
     String newTaskDescription = input.nextLine();
@@ -50,7 +52,6 @@ Task addTask(int id) {
     Task newTask = new Task(id, newTaskDescription);
 
     System.out.println("Task created!\n");
-    input.close();
     return newTask;
 
 }
@@ -65,10 +66,12 @@ void viewTask(ArrayList<Task> taskList, int id) {
 // Displays every task the list
 void viewAllTasks(ArrayList<Task> taskList) {
 
-    System.out.println("\nAll Tasks:\n");
+    System.out.println("\nAll Tasks:");
 
     for (int i = 0; i < taskList.size(); i++) {
         System.out.println((i + 1) + ". " + taskList.get(i).description);
     }
+
+    System.out.println();
 
 }
